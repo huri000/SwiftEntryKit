52150e3eb82cb2c4dbc5993c4ca8fa63fb7f3706
Max Cobb <5754073+maxxfrazer@users.noreply.github.com>
2019-06-23 18:59:51 -0400

    changed dependency to huri000/QuickLayout

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     .library(name: "SwiftEntryKit", targets: ["SwiftEntryKit"])
   ],
   dependencies: [
-    .package(url: "https://github.com/maxxfrazer/QuickLayout", .branch("master"))
+    .package(url: "https://github.com/huri000/QuickLayout", .branch("master"))
   ],
   targets: [
     .target(
