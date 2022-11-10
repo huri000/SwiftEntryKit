9f58e89472ba6a63b4710eb52b770a8fa662326f
nExmond <rnpfr254@gmail.com>
2022-11-10 16:02:26 +0900

    Use at least one scene even if the conditions are not met

--- Source/Infra/EKWindow.swift
+++ Source/Infra/EKWindow.swift
@@ -15,7 +15,9 @@ class EKWindow: UIWindow {
     init(with rootVC: UIViewController) {
         if #available(iOS 13.0, *) {
             // TODO: Patched to support SwiftUI out of the box but should require attendance
-            if let scene = UIApplication.shared.connectedScenes.filter({$0.activationState == .foregroundActive}).first as? UIWindowScene {
+            let scenes = UIApplication.shared.connectedScenes
+            let candidateScene = scenes.first { $0.activationState == .foregroundActive } ?? scenes.first
+            if let scene = candidateScene as? UIWindowScene {
                 super.init(windowScene: scene)
             } else {
                 super.init(frame: UIScreen.main.bounds)
